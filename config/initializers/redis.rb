ae380344032ec1892434a1136f9b5134fee1db36
Ivan Dobrelya <ivan.dobrelya@masterofcode.com>
2025-04-25 17:13:13 +0300

    Do not use redis in test environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(host: Rails.application.credentials.redis[:host], port: Rails.application.credentials.redis[:port])
+unless ENV['RACK_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test'
+  $redis = Redis.new(host: Rails.application.credentials.redis[:host], port: Rails.application.credentials.redis[:port])
+end
